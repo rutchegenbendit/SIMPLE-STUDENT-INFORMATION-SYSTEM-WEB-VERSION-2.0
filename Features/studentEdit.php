a0160f4a80165eca0c0a0687524719a6bc67e148
Vincent Vidal <102469596+vincentvidal23@users.noreply.github.com>
2023-01-19 21:18:02 +0800

    Update studentEdit.php

--- Features/studentEdit.php
+++ Features/studentEdit.php
@@ -9,3 +9,29 @@ if (empty($_SESSION['logged_in'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Student Info</title>
+
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
+
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+
+    <!-- Boostrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+
+    <!-- Core theme CSS-->
+    <link href="assets/css/style.css" rel="stylesheet" />
+
+    <!-- Jquery scripts -->
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+</head>
