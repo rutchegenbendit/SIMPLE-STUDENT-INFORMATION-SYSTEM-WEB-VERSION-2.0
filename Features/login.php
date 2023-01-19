5e7030b5aece87f4fa1cfff6a9ddb9037d520cd8
Vincent Vidal <102469596+vincentvidal23@users.noreply.github.com>
2023-01-19 21:00:30 +0800

    Update login.php

--- Features/login.php
+++ Features/login.php
@@ -31,3 +31,29 @@ include("server.php");
 
     <!-- Core theme JS-->
 </head>
+
+<body>
+    <main>
+        <h1>Login</h1>
+
+        <form method="post" action="login.php">
+            <div class="wrapper-input">
+                <input type="text" name="username" placeholder="Username" required>
+                <i class="fa fa-user fa-lg icon"></i>
+            </div>
+
+            <div class="wrapper-input">
+                <input type="password" name="password" placeholder="Password" required>
+                <i class="fa fa-key fa-lg icon"></i>
+            </div>
+
+            <button class="btn-login" id="btn" type="submit" name="login">Log in</button>
+        </form>
+    </main>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
