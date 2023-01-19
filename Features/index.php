a4b4bce43c19f69631e88327cd483ab6ae83546d
Vincent Vidal <102469596+vincentvidal23@users.noreply.github.com>
2023-01-19 20:54:03 +0800

    Update index.php

--- Features/index.php
+++ Features/index.php
@@ -38,3 +38,67 @@ if (empty($_SESSION['logged_in'])) {
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
+
+<body>
+    <main>
+        <a href="addStudent.php" class="add-btn">Add Student</a>
+
+        <div class="table-container">
+            <table id="table">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Course</th>
+                        <th>College</th>
+                        <th>Year</th>
+                        <th>Gender</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $query = "SELECT * FROM student";
+                    $result = mysqli_query($db, $query);
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['id'];
+
+                        $claim_status_color = "none";
+                        echo "  <tr>
+                                <td>{$row['displayId']}</td>
+                                <td>{$row['firstName']} {$row['lastName']}</td>
+                                <td>{$row['course']}</td>
+                                <td>{$row['college']}</td>
+                                <td>{$row['year']}</td>
+                                <td>{$row['gender']}</td> 
+                                
+                                <td>
+                                    <a href='studentView.php?id=$id' class='view-btn'>View</a>
+                                    <a href='studentRemove.php?id=$id' class='trash-btn'>Remove</a>
+                                </td>
+                            </tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        
+        <a href="server.php?logout=1" class="logout-btn">Log out</a>
+    </main>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Datatables -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
+    </script>
+</body>
+
+</html>
