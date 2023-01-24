946123788f8020f97c01a4d15bad2dca3668b8fc
Vincent Vidal <102469596+vincentvidal23@users.noreply.github.com>
2023-01-25 02:08:23 +0800

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,55 +1,157 @@
-DROP DATABASE IF EXISTS `database`;
-CREATE DATABASE IF NOT EXISTS `database`;
-USE `database`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 24, 2023 at 06:03 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
-CREATE TABLE `admin`(
-username VARCHAR(20) NOT NULL,
-password VARCHAR(100) NOT NULL,
-PRIMARY KEY(username)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-INSERT INTO `admin`(`username`, `password`)
-VALUES ('vinceandroach', 'nothing93874');
 
-CREATE TABLE `course`(
-code VARCHAR(10) NOT NULL,
-name VARCHAR(50) NOT NULL,
-college VARCHAR(45) NOT NULL,
-PRIMARY KEY(code)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `college`(
-code VARCHAR(10) NOT NULL,
-name VARCHAR(50) NOT NULL,
-PRIMARY KEY(code)
-);
+--
+-- Database: `database`
+--
 
-CREATE TABLE `students`(
-id VARCHAR(9) NOT NULL,
-firstname VARCHAR(50) NOT NULL,
-middlename VARCHAR(20) NOT NULL,
-lastname VARCHAR(20) NOT NULL,
-year INT(1) NOT NULL,
-gender VARCHAR(10) NOT NULL,
-coursecode VARCHAR(10) NOT NULL,
-collegecode VARCHAR(10),
-photo VARCHAR(500),
-PRIMARY KEY(id),
-FOREIGN KEY(coursecode) REFERENCES course(code),
-FOREIGN KEY(collegecode) REFERENCES college(code)
-);
+-- --------------------------------------------------------
 
-INSERT INTO `college`(`code`, `name`)
-VALUES ('CCS', 'College of Computer Studies'),
-	   ('CSM', 'College of Science and Mathematics'),
-       ('CASS', 'College of Arts and Social Sciences'),
-       ('COET', 'College of Engineering and Technology'),
-       ('CED', 'College of Education'),
-       ('CON', 'College of Nursing');
+--
+-- Table structure for table `admin`
+--
 
-INSERT INTO `course`(`code`, `name`, `college`)
-VALUES ('BSCS', 'Bachelor of Science in Computer Science', 'CCS'),
-	   ('BSIT', 'Bachelor of Science in Information Technology', 'CCS'),
-       ('BSIS', 'Bachelor of Science in Information System', 'CCS'),
-       ('BSCE', 'Bachelor of Science in Civil Engineering', 'COET'),
-       ('BSChemEng', 'Bachelor of Science in Chemical Engineering', 'CCS');
+CREATE TABLE `admin` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('vinceandroach', 'nothing93874');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `college`
+--
+
+CREATE TABLE `college` (
+  `code` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `college`
+--
+
+INSERT INTO `college` (`code`, `name`) VALUES
+('CASS', 'College of Arts and Social Sciences'),
+('CCS', 'College of Computer Studies'),
+('CED', 'College of Education'),
+('COET', 'College of Engineering and Technology'),
+('CON', 'College of Nursing'),
+('CSM', 'College of Science and Mathematics');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `code` varchar(10) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `college` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`code`, `name`, `college`) VALUES
+('BSCE', 'Bachelor of Science in Civil Engineering', 'COET'),
+('BSChemEng', 'Bachelor of Science in Chemical Engineering', 'CCS'),
+('BSCS', 'Bachelor of Science in Computer Science', 'CCS'),
+('BSIS', 'Bachelor of Science in Information System', 'CCS'),
+('BSIT', 'Bachelor of Science in Information Technology', 'CCS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` varchar(9) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `year` int NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `coursecode` varchar(10) NOT NULL,
+  `collegecode` varchar(10) DEFAULT NULL,
+  `photo` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `firstname`, `middlename`, `lastname`, `year`, `gender`, `coursecode`, `collegecode`, `photo`) VALUES
+('2019-0001', 'Neil', 'Jasperson', 'Silvano', 4, 'Male', 'BSCE', 'COET', 'http://res.cloudinary.com/dgfkh9t9q/image/upload/v1674457315/dxeayeepejd5kz0odyge.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `college`
+--
+ALTER TABLE `college`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `coursecode` (`coursecode`),
+  ADD KEY `collegecode` (`collegecode`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`coursecode`) REFERENCES `course` (`code`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`collegecode`) REFERENCES `college` (`code`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
